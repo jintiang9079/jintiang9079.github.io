8f3714a4efa6be637f3df4e11aa4ea223dc96fc3
jintiang9079 <jintiang@nycstudents.net>
2024-09-27 13:22:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jintian G"; // update the part BETWEEN the "quotes"
+let userName = "jintiang9079"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
